753f6e68e823f24c163ae26ea1b379d4772315df
DBruceLee <1204504782@qq.com>
2018-06-06 14:15:28 +0800

    服务网关

---
+++ api-gateway/src/main/java/com/dbstar/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.dbstar.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+// 开启zuul功能
+@EnableZuulProxy
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
